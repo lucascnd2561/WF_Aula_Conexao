fa1c05ccb13345805c7dc64729ca706812bf7e1f
LUCAS DA SILVA CAMILO <lucas.scamilo2@senacsp.edu.br>
2025-04-17 18:51:25 -0300

    feat: Conexao com sql e commads

--- WF_Aula/Form1.Designer.cs
+++ WF_Aula/Form1.Designer.cs
@@ -35,6 +35,10 @@
             this.lblEmail = new System.Windows.Forms.Label();
             this.txtTelefone = new System.Windows.Forms.TextBox();
             this.lblTelefone = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstContatos = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // lblNome
@@ -45,7 +49,6 @@
             this.lblNome.Size = new System.Drawing.Size(35, 13);
             this.lblNome.TabIndex = 0;
             this.lblNome.Text = "Nome";
-      
             // 
             // txtNome
             // 
@@ -53,7 +56,6 @@
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(110, 20);
             this.txtNome.TabIndex = 1;
-           
             // 
             // btnSalvar
             // 
@@ -97,11 +99,50 @@
             this.lblTelefone.TabIndex = 6;
             this.lblTelefone.Text = "Telefone";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(677, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(128, 48);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(543, 20);
+            this.txtBuscar.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Buscar Contato:";
+            // 
+            // lstContatos
+            // 
+            this.lstContatos.HideSelection = false;
+            this.lstContatos.Location = new System.Drawing.Point(133, 91);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.Size = new System.Drawing.Size(619, 205);
+            this.lstContatos.TabIndex = 11;
+            this.lstContatos.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(270, 334);
+            this.ClientSize = new System.Drawing.Size(813, 334);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstContatos);
             this.Controls.Add(this.lblTelefone);
             this.Controls.Add(this.txtTelefone);
             this.Controls.Add(this.lblEmail);
@@ -126,6 +167,10 @@
         private System.Windows.Forms.Label lblEmail;
         private System.Windows.Forms.TextBox txtTelefone;
         private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lstContatos;
     }
 }
 
--- WF_Aula/Form1.cs
+++ WF_Aula/Form1.cs
@@ -18,6 +18,20 @@ namespace WF_Aula
         public Form1()
         {
             InitializeComponent();
+
+            // configurações
+            lstContatos.View = View.Details;
+            lstContatos.LabelEdit = true;
+            lstContatos.AllowColumnReorder = true;
+            lstContatos.FullRowSelect = true;
+            lstContatos.GridLines = true;
+
+
+            // Posição dos Cabeçalhos a serem exibidos na tela
+            lstContatos.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Nome", 150, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Email", 150, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Telefone", 150, HorizontalAlignment.Left);
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
@@ -25,15 +39,30 @@ namespace WF_Aula
             try
             {
                 Conexao = new MySqlConnection(data_source);  // Usa  a string de conexão para acessar com os parametros
-                
+                Conexao.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao;
+
+                // SQL que desejamos executar - marcadores
+                cmd.CommandText =
+                    "INSERT INTO contato " +
+                    "(nome, email, telefone) " +
+                    "VALUES" +
+                    "(@nome, @email, @telefone)";
+
+                cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+
+                cmd.ExecuteNonQuery();
                 // Insere os valores de formulario dentro do banco de dados
-                string sql = "INSERT INTO contato (nome, email, telefone)" + "VALUES" + "(' " + txtNome.Text + " ' , ' " + txtEmail.Text + " ' , ' " + txtTelefone.Text + " ')";
+                //string sql = "INSERT INTO contato (nome, email, telefone)" + "VALUES" + "(' " + txtNome.Text + " ' , ' " + txtEmail.Text + " ' , ' " + txtTelefone.Text + " ')";
 
-               
-                MySqlCommand INSERT = new MySqlCommand(sql, Conexao);
-                Conexao.Open(); // Abre a Conexão
 
-                INSERT.ExecuteReader();
+                //MySqlCommand INSERT = new MySqlCommand(sql, Conexao);
+                //Conexao.Open(); // Abre a Conexão
+
+                //INSERT.ExecuteReader();
 
                 MessageBox.Show("Dados inseridos com Sucesso !!!");
             } catch(Exception ex)
@@ -42,10 +71,60 @@ namespace WF_Aula
                 MessageBox.Show(ex.Message);
             }
         }
-            
 
-       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
 
-       
+                string q = "%" + txtBuscar.Text + "%";
+
+                // Criar a conexão com o MySQL
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "SELECT * FROM contato WHERE nome LIKE @q OR email LIKE @q";
+
+
+                Conexao.Open();
+
+                // Buscar as informações
+                MySqlCommand buscar = new MySqlCommand(sql, Conexao);
+                buscar.Parameters.AddWithValue("@q", q);
+
+                // armazena as informacoes que temos na busca para mostrar na tela
+                MySqlDataReader reader = buscar.ExecuteReader();
+
+                // lista vazia 
+                lstContatos.Items.Clear();
+
+
+                while (reader.Read())
+                {
+                    // vetor
+                    string[] linha =
+                    {
+                        // obtendo as informações do banco de dados (vetor de strings)
+                        reader.GetInt32(0).ToString(), // id
+                        reader.GetString(1),           // nome
+                        reader.GetString(2),           // email
+                        reader.GetString(3),           // telefone
+                    };
+
+                    var linha_list_view = new ListViewItem(linha);
+                    lstContatos.Items.Add(linha_list_view);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
     }
+    
 }
